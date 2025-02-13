32e74e07c7ac11f5e8c39475b72d886fb2387c0e
Hoai Nam <namman212003@gmail.com>
2025-02-14 01:46:18 +0700

    GioHangScreen.js

--- my-app/src/component/client/Header.js
+++ my-app/src/component/client/Header.js
@@ -1,110 +1,105 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Link, useNavigate} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
+const Header = () => (
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">
+        WEBSITE
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarScroll"
+        aria-controls="navbarScroll"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarScroll">
+        <ul
+          className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+          style={{ "--bs-scroll-height": "100px" }}
+        >
+          <li className="nav-item">
+            <Link to="/" className="nav-link active">
+              Trang chủ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/bieu-do" className="nav-link">
+              Biểu đồ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/gio-hang" className="nav-link">
+              Giỏ hàng
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/dang-ky" className="nav-link">
+              Đăng ký
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/dang-nhap" className="nav-link">
+              Đăng nhập
+            </Link>
+          </li>
 
-const Header = () => {
-    return (
-        <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar scroll
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarScroll"
-              aria-controls="navbarScroll"
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
               aria-expanded="false"
-              aria-label="Toggle navigation"
             >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarScroll">
-              <ul
-                className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
-                style={{ "--bs-scroll-height": "100px" }}
-              >
-                <li className="nav-item">
-                  <Link to={'/'} className="nav-link active">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={"/about"} className="nav-link">
-                    About
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <a href={"/"} className="nav-link">
-                    home2
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Link
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link disabled"
-                    href="#"
-                    aria-disabled="true"
-                  >
-                    Link
-                  </a>
-                </li>
-              </ul>
-              <form className="d-flex" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
-          </div>
-        </nav>
+              Link
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#" aria-disabled="true">
+              Link
+            </a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
       </div>
-    );
-};
-
-
-Header.propTypes = {
-
-};
-
+    </div>
+  </nav>
+);
 
 export default Header;
--- my-app/src/routers/ClientRouter.js
+++ my-app/src/routers/ClientRouter.js
@@ -7,70 +7,105 @@ import AboutScreen from "../screens/client/AboutScreen";
 import Header from "../component/client/Header";
 import Footer from "../component/client/Footer";
 import Banner from "../component/client/Banner";
-
+import DangNhapScreen from "../screens/client/DangNhapScreen";
+import DangKyScreen from "../screens/client/DangKyScreen";
+import BieuDoScreen from "../screens/client/BieuDoScreen";
+import GioHangScreen from "../screens/client/GioHangScreen";
 
 const ClientRouter = () => {
-    const location = useLocation();
+  const location = useLocation();
+  // add css
+  const addCssLink = (href) => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    document.head.appendChild(link);
+  };
 
-    // add css
-    const addCssLink = (href) => {
-        const link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = href;
-        document.head.appendChild(link);
-    };
-
-    const addInitialCssLinks = () => {
-       
-        addCssLink(`https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css`);
-       
-    };
-
-    addInitialCssLinks();
-
-    // chua router, page 
-    const routes = [
-        {
-            path: '/',
-            element: <HomeSreen />,
-            showFilter: false,
-            showBanner: true,
-            showHeader: true,
-            showFooter: true
-        },
-        {
-            path: '/about',
-            element: <AboutScreen />,
-            showFilter: false,
-            showBanner: true,
-            showHeader: true,
-            showFooter: true
-        }
-    ];
-
-    const currentRoute = routes.find(route =>
-        matchPath({ path: route.path, end: true }, location.pathname)
+  const addInitialCssLinks = () => {
+    addCssLink(
+      `https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css`
     );
-    
+  };
 
-    return (
-        <div>
-            <div className="headerClient">
-                <Header />
-                <Banner/>
-            </div>
-            <div className="contentClient">
-                <Routes>
-                    {routes.map((route, index) => (
-                        <Route key={index} path={route.path} element={route.element} />
-                    ))}
-                </Routes>
-            </div>
-            <div className="footerClient">
-                <Footer />
-            </div>
-        </div>
-    );
+  addInitialCssLinks();
+
+  // chua router, page
+  const routes = [
+    {
+      path: "/",
+      element: <HomeSreen />,
+      showFilter: false,
+      showBanner: true,
+      showHeader: true,
+      showFooter: true,
+    },
+    {
+      path: "/about",
+      element: <AboutScreen />,
+      showFilter: false,
+      showBanner: true,
+      showHeader: true,
+      showFooter: true,
+    },
+    {
+      path: "/dang-nhap",
+      element: <DangNhapScreen />,
+      showFilter: false,
+      // showBanner: false,
+      showHeader: false,
+      showFooter: false,
+    },
+    {
+      path: "/dang-ky",
+      element: <DangKyScreen />,
+      showFilter: false,
+      // showBanner: false,
+      showHeader: false,
+      showFooter: false,
+    },
+    {
+      path: "/bieu-do",
+      element: <BieuDoScreen />,
+      showFilter: false,
+      // showBanner: false,
+      showHeader: false,
+      showFooter: false,
+    },
+    {
+      path: "/gio-hang",
+      element: <GioHangScreen />,
+      showFilter: false,
+      // showBanner: false,
+      showHeader: false,
+      showFooter: false,
+    },
+  ];
+
+  const currentRoute = routes.find((route) =>
+    matchPath({ path: route.path, end: true }, location.pathname)
+  );
+
+  return (
+    <div>
+      <div className="headerClient container">
+        <Header />
+        <Banner />
+      </div>
+
+      <div className="contentClient">
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </div>
+
+      <div className="footerClient container">
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default ClientRouter;
